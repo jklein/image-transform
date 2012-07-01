75145d9e4c5e692dc3d621002402dbd5c4a27d7b
Jonathan Klein <jonathan.n.klein@gmail.com>
2012-06-30 22:18:08 -0400

    Spacing cleanup

--- image_transform.php
+++ image_transform.php
@@ -20,121 +20,121 @@ $allowed_img_extentions = array('.jpg', '.png', '.gif');
 // See if the user has submitted the page
 if (!empty($_POST['submit'])) {
 
-	// Validate the input URL
-	if (!filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
-		$errors['url'] = 'Please enter a valid URL of the form http://www.example.org';
-	}
+  // Validate the input URL
+  if (!filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
+    $errors['url'] = 'Please enter a valid URL of the form http://www.example.org';
+  }
 
-	// Make sure they selected a transformation (or more than one)
-	if (empty($_POST['transformation'])) {
-		$errors['transformation'] = 'Please select a transformation you want to do on the images in the specified URL';
-	}
+  // Make sure they selected a transformation (or more than one)
+  if (empty($_POST['transformation'])) {
+    $errors['transformation'] = 'Please select a transformation you want to do on the images in the specified URL';
+  }
 
-	// If we don't have any errors then let's fetch the URL and do the transformation
-	if (empty($errors)) {
+  // If we don't have any errors then let's fetch the URL and do the transformation
+  if (empty($errors)) {
 
-		// set URL and other appropriate options
-		// $url = $_POST['url'];
-		$url = 'http://www.wayfair.com/';
+    // set URL and other appropriate options
+    // $url = $_POST['url'];
+    $url = 'http://www.wayfair.com/';
 
-		// Make a directory for this hostname (if it doesn't already exist) that will contain the images we download
-		$folder_name = str_replace('.', '', parse_url($url, PHP_URL_HOST));
-		$folder_path = '/var/www/' . $folder_name;
-		if (!file_exists($folder_path)) {
-			mkdir($folder_path);
-		}
+    // Make a directory for this hostname (if it doesn't already exist) that will contain the images we download
+    $folder_name = str_replace('.', '', parse_url($url, PHP_URL_HOST));
+    $folder_path = '/var/www/' . $folder_name;
+    if (!file_exists($folder_path)) {
+      mkdir($folder_path);
+    }
 
-		// Get the HTML from the URL (this function is from simple_html_dom.php)
-		$html = file_get_html($url);
+    // Get the HTML from the URL (this function is from simple_html_dom.php)
+    $html = file_get_html($url);
 
-		// Find all of the images on the page and process them
-		foreach ($html->find('img') as $image_element) {
-			//echo 'image src:' . $image_element->src . '<br/>';
+    // Find all of the images on the page and process them
+    foreach ($html->find('img') as $image_element) {
+      //echo 'image src:' . $image_element->src . '<br/>';
 
-			// See if we are dealing with a relative image path
-			if (substr($image_element->src, 0, 4) !== 'http') {
-				if (substr($image_element->src, 0, 1) === '/') {
-					$url_to_curl = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST) . $image_element->src;
-				} else {
-					$url_to_curl = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST) . parse_url($url, PHP_URL_PATH) . '/' . $image_element->src;
-				}
-			} else {
-				$url_to_curl = $image_element->src;
-			}
+      // See if we are dealing with a relative image path
+      if (substr($image_element->src, 0, 4) !== 'http') {
+        if (substr($image_element->src, 0, 1) === '/') {
+          $url_to_curl = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST) . $image_element->src;
+        } else {
+          $url_to_curl = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST) . parse_url($url, PHP_URL_PATH) . '/' . $image_element->src;
+        }
+      } else {
+        $url_to_curl = $image_element->src;
+      }
 
 
-			//echo 'curling url:' . $url_to_curl . '<br/>';
+      //echo 'curling url:' . $url_to_curl . '<br/>';
 
-			// Curl the image so we can write it to disk and run some Image Magick commands against it
-			$ch = curl_init($url_to_curl);
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
-			$rawdata = curl_exec($ch);
-			curl_close ($ch);
+      // Curl the image so we can write it to disk and run some Image Magick commands against it
+      $ch = curl_init($url_to_curl);
+      curl_setopt($ch, CURLOPT_HEADER, 0);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+      $rawdata = curl_exec($ch);
+      curl_close ($ch);
 
-			// Hash the URL we are curling to get a unique filename for each image.
-			// This solves the case of images with the same name but with different paths
-			$image_file_name = md5($url_to_curl);
-			$image_extension = substr($url_to_curl, strrpos($url_to_curl, '.'));
+      // Hash the URL we are curling to get a unique filename for each image.
+      // This solves the case of images with the same name but with different paths
+      $image_file_name = md5($url_to_curl);
+      $image_extension = substr($url_to_curl, strrpos($url_to_curl, '.'));
 
-			// If the image extension isn't one of the allowed ones we will just go to the next iteration in the loop
-			// This prevents things like beacons from being transformed
-			if (!in_array($image_extension, $allowed_img_extentions)) {
-				continue;
-			}
+      // If the image extension isn't one of the allowed ones we will just go to the next iteration in the loop
+      // This prevents things like beacons from being transformed
+      if (!in_array($image_extension, $allowed_img_extentions)) {
+        continue;
+      }
 
-			// Create the path that we are going to write the image to
-			$path_to_image = $folder_path . '/' . $image_file_name . $image_extension;
+      // Create the path that we are going to write the image to
+      $path_to_image = $folder_path . '/' . $image_file_name . $image_extension;
 
-			//echo 'path to image:' . $path_to_image . '<br/>';
+      //echo 'path to image:' . $path_to_image . '<br/>';
 
-			if (!file_exists($path_to_image)) {
-				$fp = fopen($path_to_image,'x');
-				fwrite($fp, $rawdata);
-				fclose($fp);
-			}
+      if (!file_exists($path_to_image)) {
+        $fp = fopen($path_to_image,'x');
+        fwrite($fp, $rawdata);
+        fclose($fp);
+      }
 
-			// Now that we have the image on disk let's open it up, transform it, and write it back with a new name
-			$image = new Imagick($path_to_image);
-			$processed_image = $image->clone();
-			$processed_image->flipImage();
-			$processed_image->writeImage($folder_path . '/' . $image_file_name . '_processed' . $image_extension);
+      // Now that we have the image on disk let's open it up, transform it, and write it back with a new name
+      $image = new Imagick($path_to_image);
+      $processed_image = $image->clone();
+      $processed_image->flipImage();
+      $processed_image->writeImage($folder_path . '/' . $image_file_name . '_processed' . $image_extension);
 
-			// Populate our image array with the path to both the old and new image
-			$img_src_array[$image_element->src] = '/' . $folder_name . '/' . $image_file_name . '_processed' . $image_extension;
+      // Populate our image array with the path to both the old and new image
+      $img_src_array[$image_element->src] = '/' . $folder_name . '/' . $image_file_name . '_processed' . $image_extension;
     }
 
 
-    // At this point we should have all of the images downloaded, processed, and writted to disk.
+    // At this point we should have all of the images downloaded, processed, and written to disk.
     // Now we just need to replace the source attributes in the HTML with the paths to the new images we have created:
     foreach ($img_src_array as $old_src => $new_src) {
-    	$html = str_replace($old_src, $new_src, $html);
+      $html = str_replace($old_src, $new_src, $html);
     }
 
-	} else {
-		print_r($errors);
-	}
+  } else {
+    print_r($errors);
+  }
 }
 
 ?>
 
 <DOCTYPE html>
 <html>
-	<head>
-		<title>Image Filtering</title>
-		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-	</head>
-	<body>
-		<form method="post" action="<?=$_SERVER['SCRIPT_NAME'];?>">
-			<input type="url" value="" name="url" placeholder="URL Here" />
-			<select name="transformation" multiple="multiple">
-				<option value="flip">Flip along x-axis (vertical flip)</option>
-				<option value="gray">Convert Images to grayscale</option>
-			</select>
-			<input type="submit" value="submit" name="submit" />
-		</form>
+  <head>
+    <title>Image Filtering</title>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+  </head>
+  <body>
+    <form method="post" action="<?=$_SERVER['SCRIPT_NAME'];?>">
+      <input type="url" value="" name="url" placeholder="URL Here" />
+      <select name="transformation" multiple="multiple">
+        <option value="flip">Flip along x-axis (vertical flip)</option>
+        <option value="gray">Convert Images to grayscale</option>
+      </select>
+      <input type="submit" value="submit" name="submit" />
+    </form>
 
-		<?=$html;?>
-	</body>
+    <?=$html;?>
+  </body>
 </html>
